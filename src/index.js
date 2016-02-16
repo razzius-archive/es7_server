0572cf2670902feaeea3e0e647597c540367fa08
Razzi Abuissa <razzi53@gmail.com>
2016-02-15 18:11:45 -0800

    Simple async server example

---
+++ src/index.js
@@ -0,0 +1,23 @@
+require("babel-polyfill")
+
+import express from 'express'
+import request from 'request'
+import promisify from 'promisify-node'
+
+
+const get = promisify(request.get)
+
+async function razziNet() {
+  const response = await get('http://abuissa.net')
+  return response.body
+}
+
+const app = express()
+
+
+app.get('/', async (req, res) => {
+  res.send(await razziNet())
+})
+
+
+app.listen(3000)
